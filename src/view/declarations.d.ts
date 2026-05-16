b9ce14eccc4684947ccf5b1d6b241a6404fae686
vlado-1 <vladosmolovic18@yahoo.com>
2026-05-16 18:40:08 +1000

    issue #1: Fix typescript compile issues.

--- src/view/declarations.d.ts
+++ src/view/declarations.d.ts
@@ -11,3 +11,27 @@ declare module "*.jpg" {
     const value: { default: string };
     export default value;
 }
+
+declare module "*.css" {
+    const content: string;
+    export default content;
+}
+
+declare module 'reactjs-popup' {
+  import * as React from 'react';
+
+  export type PopupChildren = (close: () => void) => React.ReactNode;
+
+  // Omit 'children' from HTMLAttributes so we can allow a function render-prop.
+  export interface PopupProps
+    extends Omit<React.HTMLAttributes<HTMLElement>, 'children'> {
+    children?: React.ReactNode | PopupChildren;
+    modal?: boolean;
+    open?: boolean;
+    onClose?: () => void;
+    trigger?: React.ReactNode | PopupChildren;
+  }
+
+  const Popup: React.ComponentType<PopupProps>;
+  export default Popup;
+}
